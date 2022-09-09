1bff62fe41052be56baf18a924bf7f968c7c6e59
Louis-Marie Kraft <lm.kraft@outlook.fr>
2022-09-09 12:35:46 +0200

    action-sample initial comit

---
+++ action-sample/src/main/resources/javascript/apps/registerExtensions.js
@@ -0,0 +1,34 @@
+window.jahia.i18n.loadNamespaces('action-sample');
+
+window.jahia.uiExtender.registry.add('callback', 'action-sampleExample', {
+    targets: ['jahiaApp-init:60'],
+    callback: function () {
+        window.jahia.uiExtender.registry.add('adminRoute', 'action-sampleExample', {
+            targets: ['administration-sites:999', 'action-sampleaccordion'],
+            label: 'action-sample:label.settings.title',
+            icon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            isSelectable: true,
+            requireModuleInstalledOnSite: 'action-sample',
+            iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.action-sample.html.ajax'
+        });
+
+        window.jahia.uiExtender.registry.add('action', 'action-sampleExample', {
+            buttonIcon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            buttonLabel: 'action-sample:label.action.title',
+            targets: ['contentActions:999'],
+            onClick: context => {
+                window.open('https://github.com/Jahia/app-shell/blob/master/docs/declare-new-module.md', "_blank");
+            }
+        });
+
+        window.jahia.uiExtender.registry.add('accordionItem', 'action-sampleApps_Example', window.jahia.uiExtender.registry.get('accordionItem', 'renderDefaultApps'), {
+            targets: ['jcontent:998'],
+            label: 'action-sample:label.appsAccordion.title',
+            icon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
+            appsTarget: 'action-sampleaccordion',
+            isEnabled: function(siteKey) {
+                return siteKey !== 'systemsite'
+            }
+        });
+    }
+});
